748c5b9df15a0e2236c565decd1ee44fcfe53987
a11yus <socialhunt9@gmail.com>
2022-09-15 13:01:49 +0530

    hogaya

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 1947;
 require('dotenv').config()
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('data.json');
 server.use(middlewares);
 server.use(router);
 server.listen(port, console.log(`listning on ${port}`));
